1527fc0bcf5cfc355f0a15c2c1f4158252d5413d
BQcThang <quocthang6a1@gmail.com>
2024-05-30 02:28:56 +0700

    login signup fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,32 +1,42 @@
+import { getAjax, postAjax } from './api-ajax.js';//import các hàm getAjax và postAjax từ file api-ajax.js
+
 $(document).ready(function () {
-    $("loginBtn").click(function () {
+    $("#loginBtn").click(function () {
 
         var username = $("#login-username").val();
         var password = $("#login-password").val();
+
+        console.log(username, password);
 
         if (!username || !password) {
             swal("Failed", "Username and password are required!", "warning");
             return;
         }
 
-        $.ajax({
-            type: "POST",
-            url: "your-login-API",
-            data: {
+        postAjax("user/login",
+            {
                 username: username,
                 password: password
             },
-            success: function (response){
-                var token = response.toke;
-                window.location.href = "index.html";
-            },
-            error: function (xhr, status, error){
-                swal("Failed", "Incorrect username or password!", "error");
+        ).done(function (response) {
+            // Khi gọi API thì kết quả sẽ trả ở đây
+            var token = msg.data
+            //check xem token tồn tại chưa và có rỗng hay không
+            if (token != null && token != "") {
+                swal("Success", "Login successfully!", "success") // xài thư viện sweetalert2 để hiển thị thông báo
+                    .then(() => {
+                        //lưu token bảo mật vào bộ nhớ của browser
+                        localStorage.setItem("token", token)
+                        //chuyển qua trang index.html
+                        window.location.href = "index.html"
+                    });
             }
-        });
-    });
+        }).fail(function (jqXHR) {
+            swal("Failed!", "Incorrect username or password!", "warning");
+        })
+    })
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#registerBtn").click(function () {
             var username = $("#register-username").val();
             var password = $("#register-password").val();
@@ -34,40 +44,28 @@ $(document).ready(function () {
             var fullname = $("#register-fullname").val();
             var phoneNumber = $("#register-phone-number").val();
 
-            if (!username || !password || !email || !fullname || !phoneNumber) {
-                Swal.fire({
-                    icon: 'warning',
-                    title: 'Failed',
-                    text: 'All fields are required!'
-                });
+            if (password != repeatPassword) {
+                swal("Error!", "Password are not the same!")
                 return;
             }
 
-            $.ajax({
-                type: "POST",
-                url: "your-register-API",
-                data: {
+            postAjax("user/register",
+                {
                     username: username,
                     password: password,
                     email: email,
                     fullname: fullname,
                     phoneNumber: phoneNumber
                 },
-                success: function (response) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Success',
-                        text: 'Registration successful!'
+            ).done(function (msg) {
+                swal("Success", "Register successfully!", "success")
+                    .then(() => {
+                        window.location.href = "login.html"
                     });
-                },
-                error: function (xhr, status, error) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Failed',
-                        text: 'Registration failed!'
-                    });
-                }
-            });
-        });
-    });
-});
+            })
+                .fail(function () {
+                    swal("Failed!", "Register failed!", "warning");
+                });
+        })
+    })
+})
